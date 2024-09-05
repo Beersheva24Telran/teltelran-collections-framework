4df01d06135f883c5092ea125d2ee60e9693ee6e
Yuragranovsky <yuragranovsky@gmail.com>
2024-09-05 13:46:29 +0300

    HW #25 solution

--- src/test/java/telran/util/TreeMapTest.java
+++ src/test/java/telran/util/TreeMapTest.java
@@ -1,12 +1,18 @@
 package telran.util;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.BeforeEach;
 
 public class TreeMapTest extends AbstractMapTest {
 
     @Override
     <T> void runTest(T[] expected, T[] actual) {
-       //TODO
+       T[] expectedSorted = Arrays.copyOf(expected, expected.length);
+       Arrays.sort(expectedSorted);
+       assertArrayEquals(expectedSorted, actual);
 
     }
     
